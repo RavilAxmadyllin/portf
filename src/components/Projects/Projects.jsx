ba5377a005d3547b4f9a9b2d7e543fd0b970c5b1
RavilAxmadyllin <craftwaymiks@gmail.com>
2020-05-23 03:35:42 +0300

    test

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import styles from'./Projects.module.css';
 import Title from "../Block/Title";
 import Work from "./Work";
-
+import classes from '../Block/ContainerClass.module.css'
 function Project() {
    return(
        <div className={styles.projectsWrap}>
-           <div className="container">
+           <div className={classes.container}>
                <Title title={'My work'}/>
                <div className={styles.projects}>
                        <Work title={'social-network'} discription={'React Redux'}/>
